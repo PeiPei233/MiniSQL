335f9fec7ee6b129948e0a9cc00205c0ed77129d
PeiPei233 <2503585731@qq.com>
2023-06-06 10:47:56 +0800

    bug fixed: delete executor

--- src/executor/delete_executor.cpp
+++ src/executor/delete_executor.cpp
@@ -30,7 +30,10 @@ bool DeleteExecutor::Next([[maybe_unused]] Row *row, RowId *rid) {
     std::vector<IndexInfo *> indexes;
     exec_ctx_->GetCatalog()->GetTableIndexes(plan_->GetTableName(), indexes);
     for (auto index : indexes) {
-      res = index->GetIndex()->RemoveEntry(child_row, child_rid, exec_ctx_->GetTransaction());
+      Row index_row;
+      child_row.GetKeyFromRow(table_info->GetSchema(), index->GetIndexKeySchema(), index_row);
+      std::cout << "index row field count: " << index_row.GetFieldCount() << " index schema field count: " << index->GetIndexKeySchema()->GetColumnCount() << std::endl;
+      res = index->GetIndex()->RemoveEntry(index_row, child_rid, exec_ctx_->GetTransaction());
     }
     // update the table
     table_info->GetTableHeap()->ApplyDelete(child_rid, exec_ctx_->GetTransaction());
