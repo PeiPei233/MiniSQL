fffbbf7abaa2e22bb69384c24940b95f0da85ad0
PeiPei233 <2503585731@qq.com>
2023-06-07 20:44:19 +0800

    bug fixed: false node size

--- src/include/page/b_plus_tree_leaf_page.h
+++ src/include/page/b_plus_tree_leaf_page.h
@@ -27,9 +27,8 @@
 #include "index/generic_key.h"
 #include "page/b_plus_tree_page.h"
 
-#define MappingType std::pair<GenericKey *, RowId>
 #define LEAF_PAGE_HEADER_SIZE 32
-#define LEAF_PAGE_SIZE (((PAGE_SIZE - LEAF_PAGE_HEADER_SIZE) / sizeof(MappingType)) - 1)
+#define LEAF_PAGE_SIZE (((PAGE_SIZE - LEAF_PAGE_HEADER_SIZE) / (processor_.GetKeySize() + sizeof(RowId))) - 1)
 
 class BPlusTreeLeafPage : public BPlusTreePage {
  public:
